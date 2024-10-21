80b126f2fca41e2a393eb10fbc5f44e548c8ad6c
crowmancer <75182686+crowmancer@users.noreply.github.com>
2024-10-21 15:31:18 -0500

    le commit

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+	<PrimePanel header="About Me" class="my-6">
+		i said only partly
+		<PrimeButton label="Check" icon="pi pi-check" />
+	</PrimePanel>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+	<PrimePanel header="Welcome!" class="my-6">
+		she said tell me do u luv me
+		<PrimeButton label="Check" icon="pi pi-check" />
+	</PrimePanel>
+</template>
